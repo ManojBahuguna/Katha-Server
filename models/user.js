e853944da0154e9660aa910af39efbca4f0a9808
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-04 23:18:02 +0530

    Created User Model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+const userSchema = new Schema({
+  username: { type: string, required: true, unique: true },
+  email: { type: string, required: true, unique: true },
+  password: { type: string, required: true },
+  firstName: string,
+  lastName: string
+})
+
+const User = module.exports = mongoose.model('User', userSchema);
+
+module.exports.getById = User.findById;
+
+module.exports.getByUsername = username => User.findOne({ username })
+
+module.exports.add = user => bcrypt.genSalt().then(salt =>
+  bcrypt.hash(user.password, salt).then(hashedPassword =>
+    new User({
+      email: user.email,
+      password: hashedPassword
+    }).save()
+  )
+)
+
+module.exports.comparePassword = bcrypt.compare
