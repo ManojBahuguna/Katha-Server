89dd8b5aff5ae43710f07806b9a51a656f05e45b
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-04 18:54:11 +0530

    Setup Mongoose

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  DB_URL: process.env.DB_URL || 'mongodb://localhost/katha'
+}
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+// IMPORTS
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 8080
-
-
-// Serve public folder
 const path = require('path')
+const { PORT, DB_URL } = require('./config')
+const mongoose = require('mongoose')
+
+
+
+// ROUTES
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-// Start Server
+
+
+// SETUP MONGOOSE
+mongoose.connect(DB_URL).then(() => {
+  console.log(`Successfully connected to database: ${DB_URL}`)
+}).catch(err => {
+  console.log(`Error connecting to database: ${err}`)
+});
+
+
+
+// START SERVER
 app.listen(PORT, () => {
-	console.log(`Server started at PORT: ${PORT}`)
+  console.log(`Server started at PORT: ${PORT}`)
 })
